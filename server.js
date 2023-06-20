6f089653cd93ba77434b89ba2c76b9262bdd1c58
EddyShimwa <shedpray16@gmail.com>
2023-06-20 16:22:00 +0200

    add the table to hold user data

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const bodyparser = require('body-parser');
+const path = require('path');
 
 
 const app = express();
@@ -26,7 +27,7 @@ app.use('/img', express.static(path.resolve(__dirname, 'asssets/img')))
 app.use('/js', express.static(path.resolve(__dirname, 'asssets/js')))
 
 app.get('/', (req, res) => {
-    res.send('Hey node am back');
+    res.render('index');
 })
 
 app.listen(PORT, () => {console.log(`Server is running on http://localhost:${PORT}`)});
